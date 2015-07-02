121d307d17179e728830e8a929ab3ec160e03b87
Zhou Li <zhoulijoe@gmail.com>
2015-07-01 23:27:38 -0700

    Re-organize and clean roles

--- roles/mongodb/files/mongorc.js
+++
@@ -1,9 +0,0 @@
-function insertData(dbName, colName, num) {
-  var col = db.getSiblingDB(dbName).getCollection(colName);
-
-  for (i = 0; i < num; i++) {
-    col.insert({x:i});
-  }
-
-  print(col.count());
-}
